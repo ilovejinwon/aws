6f2fed69a94eb125a19c8b5c1835d1ede6b3e8c1
ilovejinwon <mminsoo@gmail.com>
2018-10-03 23:52:36 +0900

    Web Post 조회기능 추가

--- src/main/java/com/example/aws/web/WebRestController.java
+++ src/main/java/com/example/aws/web/WebRestController.java
@@ -1,13 +1,24 @@
 package com.example.aws.web;
 
+import com.example.aws.domain.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@RestController
+
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
