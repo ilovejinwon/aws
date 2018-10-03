bc3369a9c04849d992bffb4a6e094dcc3bb39aa5
ilovejinwon <mminsoo@gmail.com>
2018-10-04 00:19:40 +0900

    WebRestController 오류 수정

--- src/main/java/com/example/aws/web/WebRestController.java
+++ src/main/java/com/example/aws/web/WebRestController.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @AllArgsConstructor
+@RestController
 public class WebRestController {
 
     private PostsRepository postsRepository;
@@ -20,5 +22,10 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
         postsRepository.save(dto.toEntity());
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "working";
     }
 }
